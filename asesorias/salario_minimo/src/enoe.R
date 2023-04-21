fa18d92b2e293724841ca7d94992a6b366a52a1a
milocortes <hermilocg@hotmail.com>
2023-04-20 21:56:10 -0600

    equipo salario

--- asesorias/salario_minimo/src/enoe.R
+++ asesorias/salario_minimo/src/enoe.R
@@ -10,5 +10,26 @@ setwd("/home/milo/Documents/egtp/clases/licenciatura/ccm-2023/mod_04_concentraci
 ###################Cargado de Bases#######################################
 
 sdemt319 <- read.csv("ENOEN_SDEMT322.csv")
+minimal_sdemt <- sdemt319[,c("hrsocup","ingocup","ing_x_hrs","fac_tri")]
 
+minimal_sdemt <- read.csv("minimal_sdemt.csv")
+write.csv(minimal_sdemt,"minimal_sdemt.csv", row.names = FALSE)
+########################### seleccionamos poblacion 15 aÃ±os o mas ############3
+sdemt319$eda <- as.numeric(as.character(sdemt319$eda) )
+sdemt319 <- subset (sdemt319, eda >= 15 & eda <= 98) ###
 
+########################## Incorporamos el esquema de muestreo #############
+
+mydesign<-svydesign(id=~upm, strata=~est_d_tri, weight=~fac_tri, data=sdemt319, nest=TRUE)
+options(survey.lonely.psu="adjust")
+
+domestico_df <- subset(sdemt319, domestico == 8 | domestico == 3  )
+
+## Horas promedio trabajadas
+wtd.mean(domestico_df$hrsocup, domestico_df$fac_tri)
+## Ingreso promedio mensual
+wtd.mean(domestico_df$ingocup, domestico_df$fac_tri)
+## Ingreso promedio
+wtd.mean(domestico_df$ing_x_hrs, domestico_df$fac_tri)
+#
+cruza_uno <- wtd.table(domestico_df$domestico, domestico_df$imssissste, domestico_df$fac_tri)
